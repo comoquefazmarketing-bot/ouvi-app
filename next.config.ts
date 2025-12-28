969590f00deb9cb7b6ba5bc0de265c3aea65e7aa
Felipe Makarios <comoquefazmarketing@gmail.com>
2025-12-28 14:23:03 -0300

    fix: tipagem obrigatoria do children

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! ATENÇÃO !!
-    // Isso permite que o deploy termine mesmo com erros de TypeScript.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Também ignora erros de "limpeza" de código (linting)
     ignoreDuringBuilds: true,
   },
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 ﻿import "./globals.css";
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-br">
       <body className="bg-black text-white">{children}</body>
