0d3a506f818964b5fea945c07d02eb9789c9b86d
Felipe Makarios <comoquefazmarketing@gmail.com>
2025-12-31 00:23:42 -0300

    Fix: limpando imports fantasmas no dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
+import PostCard from "@/components/dashboard/Post/PostCard";
 import FeedList from "@/components/dashboard/Feed/FeedList";
 
 export default function DashboardPage() {
-  const [session, setSession] = useState<any>(null);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-  }, []);
-
   return (
-    <div style={{ width: "100%" }}>
-      {/* O FeedList já cuida de renderizar os FeedItems (PostCards) */}
+    <div style={{ pb: '100px' }}>
       <FeedList />
     </div>
   );
